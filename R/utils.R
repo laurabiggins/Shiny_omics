336865a12447059960da5f0892964deb078b97ce
laurabiggins <laurabiggins10@gmail.com>
2021-09-22 13:20:20 +0100

    Updated aesthetics.

---
+++ R/utils.R
@@ -0,0 +1,81 @@
+#' Set DT::datatable options
+#'
+#' @param data  A tibble or dataframe. 
+#' @param n_rows The number of rows to display (integer).
+#' @param lineHeight The height of each row in table (percentage). Default "80%". 
+#' @param dom_opt dom options for table components: 
+#' * l - length changing input control
+#' * f - filtering input
+#' * t - table
+#' * i - table info 
+#' * p - pagination     
+#'  Any combination of these options should be supplied as a single character 
+#'  string and will be displayed in the order supplied. 
+#'  Default: "tip" (table, info, pagination).
+#' @param cols_to_round columns to round (vector of column numbers). 
+#'  See `sig_digits`. Default: NULL 
+#' @param dt_options list of options to supply to `datatable()`.
+#'  The default is NULL, meaning options are taken from the relevant supplied parameters, 
+#'  i.e. `dom`, `pageLength = n_rows`, `lengthMenu = table_lengths`. 
+#'  This argument is for if further customisation is required.
+#' @param sig_digits  The number of significant digits to round to if `cols_to_round` 
+#'  is not NULL. Default: 3
+#' @param regex If `TRUE`, this allows regex searchng of the table e.g. gene1 | gene2.
+#' @param selection Whether to allow multiple rows to be selected at once, or just a 
+#'  single row. Either `"multiple"` or `"single"`. Default: `"single"`
+#' @param table_lengths Integer vector of available table lengths. 
+#'  Default: c(10,20,50,100). This will only show if dom_opt includes "l".
+#' @param filter_pos Location of column filters. One of c("none", "bottom", "top"). 
+#'  Default: "none"
+#' @param show_rownames Whether to show rownames or not. Default: FALSE
+#' @return DT::datatable() object
+#' @export
+#' @md
+#' @examples
+#' dt_setup(iris)
+dt_setup <-  function(data, 
+                      n_rows = 10, 
+                      lineHeight = "50%", 
+                      dom_opt = "tip", 
+                      cols_to_round = NULL, 
+                      dt_options = NULL, 
+                      sig_digits = 3, 
+                      regex = FALSE, 
+                      selection = "single", 
+                      table_lengths = c(10,20,50,100), 
+                      filter_pos = "none",
+                      show_rownames = FALSE,
+                      style = "bootstrap4") {
+  
+  assertthat::assert_that(
+    tibble::is_tibble(data) | base::is.data.frame(data) | base::is.matrix(data), 
+    msg = "data supplied to dt_setup must be a tibble or data frame"
+  )
+  
+  if (is.null(dt_options)) {
+    dt_options = list(dom = dom_opt, 
+                      pageLength = n_rows,
+                      lengthMenu = table_lengths)
+  }
+  
+  if (regex) {
+    dt_options[["search"]] <- list(regex = TRUE, caseInsensitive = TRUE)
+  }
+  
+  dt_table <- DT::datatable(
+    data,
+    style = style,
+    rownames = show_rownames,
+    escape   = FALSE,
+    filter   = filter_pos,
+    options  = dt_options,
+    selection = selection
+    
+  ) %>%
+    DT::formatStyle(0, target = 'row', lineHeight = lineHeight)
+  
+  if (!is.null(cols_to_round)) {
+    dt_table <- DT::formatRound(dt_table, cols_to_round, sig_digits)
+  }  
+  dt_table   
+}
--- app.R
+++ app.R
@@ -22,129 +22,152 @@ conditions <- c("Naive", "Naive+PRC2i", "Primed", "Primed+PRCi")
 acid_plot_height <- "250px"
 
 datatypes <- c(
-    "Gene expression" = "gene_expr", 
-    "Acid extractome protein abundance" = "acid_protein",
-    "Chromatin-associated protein abundance" = "chr_protein",
-    "Histone modification abundance" = "histones"
+  "Gene expression" = "gene_expr", 
+  "Acid extractome protein abundance" = "acid_protein",
+  "Chromatin-associated protein abundance" = "chr_protein",
+  "Histone modification abundance" = "histones"
 )
 
 ui <- fluidPage(
 
-    shinyalert::useShinyalert(),
-    tags$head(
-        tags$link(rel = "stylesheet", type = "text/css", href = "omics.css")
-    ),
-    
-    # Application title
+  shinyalert::useShinyalert(),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "omics.css")
+  ),
+  
+  # Application title
+  wellPanel(id = "title_section", 
     titlePanel("Chromatin and histone proteomics of human pluripotent states"),
     h3("Integrated Multi-Omics Analyses Reveal Polycomb Repressive Complex 2
-Restricts Naive Human Pluripotent Stem Cell to Trophoblast Fate Induction"),
+  Restricts Naive Human Pluripotent Stem Cell to Trophoblast Fate Induction"),
     h4("Zijlmans, Talon, Verhelst, Bendall et al."),
-    br(),
-    br(),
-
-    actionButton("clear_table", label = "Clear table selections"),
+    br()
+  ),
+  wellPanel(id = "table_volcano_panel",
     fluidRow(
-        column(
-            width = 7,
-            DT::dataTableOutput("pp_table")
-        ),
-        column(
-            width = 5,
-            br(),
-            plotlyOutput("volcano")
+      column(
+        width = 7,
+        wellPanel(
+          id = "table_panel", 
+          actionButton("clear_table", label = "Clear table selections"),
+          DT::dataTableOutput("pp_table"))
+      ),
+      column(
+        width = 5,
+        wellPanel(
+          id = "volcano_panel",
+          br(),
+          plotlyOutput("volcano", height = "400px")
         )
-    ),
-    br(),
-    checkboxInput(inputId = "select_all_plots", label = "Show all data types", value = TRUE),
-    checkboxGroupInput(
-        inputId = "plot_panels_to_display",
-        label = "",
-        choices = datatypes,
-        inline = TRUE
-    ),
-    
+      )
+    )
+  ),
+  br(),
+  wellPanel(id = "plot_type_selections",
     fluidRow(
-        conditionalPanel(
-            condition = "input.plot_panels_to_display.includes('gene_expr')",
-            column(
-                width = 6,
-                uiOutput(outputId = "gene_expr", class = "plot_box")
-            )
-        ),
-        conditionalPanel(
-            condition = "input.plot_panels_to_display.includes('acid_protein')",
-            column(
-                width = 6, 
-                uiOutput("protein1", class = "plot_box")
-            )
+      column(
+        width = 2,
+        checkboxInput(inputId = "select_all_plots", label = strong("Show all data types"), value = TRUE)
+      ),
+      column(
+        width = 10,
+        checkboxGroupInput(
+          inputId = "plot_panels_to_display",
+          label = NULL,
+          choices = datatypes,
+          inline = TRUE
         )
+      )
+    )
+  ),
+  br(),
+  fluidRow(
+    conditionalPanel(
+      condition = "input.plot_panels_to_display.includes('gene_expr')",
+      column(width = 6, uiOutput(outputId = "gene_expr", class = "plot_box"))
     ),
-    fluidRow(
-        conditionalPanel(
-            condition = "input.plot_panels_to_display.includes('chr_protein')",
-            column(
-               width = 6,
-               uiOutput("protein_second", class = "plot_box")
-           )
-        ),
-        conditionalPanel(
-            condition = "input.plot_panels_to_display.includes('histones')",
-            column(
-               width = 6, 
-               uiOutput("histones", class = "plot_box")
-            )
-        )    
-   ),
-    br(),
-    checkboxGroupInput(
-        inputId = "conditions_to_display",
-        label = "",
-        choices = conditions,
-        selected = c("Naive", "Primed"),
-        inline = TRUE
+    conditionalPanel(
+        condition = "input.plot_panels_to_display.includes('acid_protein')",
+        column(width = 6, uiOutput("protein1", class = "plot_box"))
     ),
-    br(),
-    actionButton("browser", "browser")
+    conditionalPanel(
+        condition = "input.plot_panels_to_display.includes('chr_protein')",
+        column(width = 6, uiOutput("protein_second", class = "plot_box"))
+    ),
+    conditionalPanel(
+        condition = "input.plot_panels_to_display.includes('histones')",
+        column(width = 6, uiOutput("histones", class = "plot_box"))
+    ) 
+ ),
+  checkboxGroupInput(
+      inputId = "conditions_to_display",
+      label = "",
+      choices = conditions,
+      selected = c("Naive", "Primed"),
+      inline = TRUE
+  ),
+  br(),
+  actionButton("browser", "browser")
 )
 
 key <- row.names(volcano_dataset)
 
 server <- function(input, output, session) {
     
-    table_proxy <- dataTableProxy("pp_table")
-    
-    output$pp_table <- DT::renderDataTable(dataset)
-    
-    selected_ids <- reactiveVal()
-    
-    filtered_dataset <- reactive({
-        data_long %>%
-            filter(condition %in% input$conditions_to_display)
-    })
-    
-    output$volcano <- renderPlotly({
+  table_proxy <- dataTableProxy("pp_table")
   
-        p <- volcano_dataset %>%
-            ggplot(aes(x = log2fc_naive_primed, y = -log10(Naive_vs_Primed), key = key)) + #colour = species, fill = species)) +
-                geom_point(shape = 21) +
-                theme(legend.position="none")
-        
-        if(! is.null(selected_ids())) {
-            
-            selected_subset <- volcano_dataset %>%
-               filter(Accession %in% selected_ids())
-            #colours_needed <- plot_colours[1:nrow(selected_subset)]
+  output$pp_table <- DT::renderDataTable({
+    dt_setup(
+      dataset,
+      #lineHeight = "50%",
+      selection = "multiple",
+      dt_options = list(
+        dom = "ftip", 
+        columnDefs = list(
+          list(
+            targets = 1,
+            width = "600px",
+            render = JS(
+              "function(data, type, row, meta) {",
+              "return type === 'display' && data.length > 70 ?",
+              "'<span title=\"' + data + '\">' + data.substr(0, 70) + '...</span>' : data;",
+              "}"
+            )
+          )
+        )  
+      )
+    )
+  })
+  
+  selected_ids <- reactiveVal()
+  
+  filtered_dataset <- reactive({
+    data_long %>%
+      filter(condition %in% input$conditions_to_display)
+  })
+  
+  output$volcano <- renderPlotly({
 
-            p <- p + geom_point(
-                        data = selected_subset,
-                        aes(key = NULL),
-                        shape = 21,
-                        colour = "red",
-                        # fill = colours_needed, # this works with ggplot but not plotly
-                        stroke = 1,
-                        size = 3
-                    )
+    p <- volcano_dataset %>%
+          ggplot(aes(x = log2fc_naive_primed, y = -log10(Naive_vs_Primed), key = key)) + #colour = species, fill = species)) +
+            geom_point(shape = 21) +
+            theme(legend.position="none")
+      
+      if(! is.null(selected_ids())) {
+          
+          selected_subset <- volcano_dataset %>%
+             filter(Accession %in% selected_ids())
+          #colours_needed <- plot_colours[1:nrow(selected_subset)]
+
+          p <- p + geom_point(
+                    data = selected_subset,
+                    aes(key = NULL),
+                    shape = 21,
+                    colour = "red",
+                    # fill = colours_needed, # this works with ggplot but not plotly
+                    stroke = 1,
+                    size = 3
+                )
         }
             
         ggplotly(p)
@@ -162,17 +185,22 @@ server <- function(input, output, session) {
       
     if(input$select_all_plots) {
       updateCheckboxGroupInput(session, "plot_panels_to_display", selected = datatypes)
-    } else {
-      updateCheckboxGroupInput(session, "plot_panels_to_display", selected = "")
+    } 
+  })
+   
+  observeEvent(input$plot_panels_to_display, {
+    
+    if(length(input$plot_panels_to_display) != 4) {
+      updateCheckboxInput(session, "select_all_plots", value = FALSE)
     }
   })
-    
+   
   ## table row selections ----
   observeEvent(input$pp_table_rows_selected, {
       
     row_numbers <- as.numeric(input$pp_table_rows_selected)
     
-    print("updating selected rows")
+    #print("updating selected rows")
     
     if(length(row_numbers) > 4){
       shinyalert::shinyalert(title = "", text = "Maximum of 4 rows of data will be shown.")
@@ -202,57 +230,55 @@ server <- function(input, output, session) {
   })
     
     
-    # plotly events ----
+  # plotly events ----
     
-    observeEvent(event_data("plotly_doubleclick"), {
-        print("double_clicked")
-        selected_ids(NULL)
-    })
-    
-    observeEvent(event_data("plotly_click"), {
-        print("clicked")
-        d <- event_data("plotly_click")
-        req(d)
-        row_no <- as.numeric(d$key)
-        
-        selected_accessions <- volcano_dataset %>%
-            slice(row_no) %>%
-            pull(Accession)
-        
-        if(length(selected_ids()) < 4) { # add up to 4 datasets
-            selected_accessions <- c(selected_ids(), selected_accessions)
-        }    
-        selected_ids(selected_accessions)
-    })
-    
-    observeEvent(event_data("plotly_selected"), {
-        print("selected")
-        d <- event_data("plotly_selected")
-        req(d)
-        row_numbers <- as.numeric(d$key)
-        
-        if(length(row_numbers) > 4){
-            shinyalert::shinyalert(
-                title = "",
-                text = "Maximum of 4 data points will be shown. \n
-                Try zooming in if points are too close together. Double click to deselect all."
-            )
-            row_numbers <- row_numbers[1:4]
-        }
-        selected_ids(
-            volcano_dataset %>%
-                slice(row_numbers) %>%
-                pull(Accession)
-        )
-    })
-
-        
-    observeEvent(event_data("plotly_deselect"), {
-        print("none selected")
-        selected_ids(NULL)
-        #selectRows(table_proxy, selected = NULL)
-    })
+  observeEvent(event_data("plotly_doubleclick"), {
+      selected_ids(NULL)
+  })
   
+  observeEvent(event_data("plotly_click"), {
+
+    d <- event_data("plotly_click")
+    req(d)
+    row_no <- as.numeric(d$key)
+    
+    selected_accessions <- volcano_dataset %>%
+      slice(row_no) %>%
+      pull(Accession)
+    
+    if(length(selected_ids()) < 4) { # add up to 4 datasets
+      selected_accessions <- c(selected_ids(), selected_accessions)
+    }    
+    selected_ids(selected_accessions)
+  })
+  
+  observeEvent(event_data("plotly_selected"), {
+   
+    d <- event_data("plotly_selected")
+    req(d)
+    row_numbers <- as.numeric(d$key)
+    
+    if(length(row_numbers) > 4){
+      shinyalert::shinyalert(
+        title = "",
+        text = "Maximum of 4 data points will be shown. \n
+        Try zooming in if points are too close together. Double click to deselect all."
+      )
+      row_numbers <- row_numbers[1:4]
+    }
+    selected_ids(
+      slice(volcano_dataset, row_numbers) %>%
+      pull(Accession)
+    )
+  })
+
+      
+  observeEvent(event_data("plotly_deselect"), {
+    print("none selected")
+    selected_ids(NULL)
+    #selectRows(table_proxy, selected = NULL)
+  })
+
   # plot panels -----
   output$gene_expr <- renderUI({
     
